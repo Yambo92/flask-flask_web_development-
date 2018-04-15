de32d828e403f1c72797a7357dda539722e3b2ba
yambo1992 <1810962886@qq.com>
2018-04-15 20:55:33 +0800

    flask-web-dev10.4之前用户管理系统

---
+++ app/auth/forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('keep me logged in')
+    submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64),
+                                                   Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                          'Username must have only letters, numbers,dots or underscores')])
+    name = StringField('FullName', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z]+\s*\w$', 0)])
+    address = StringField('Address', validators=[DataRequired(), Length(1, 64)])
+    password = PasswordField('Password', validators=[DataRequired(),
+                                                     EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    about_me = StringField('About Me', validators=[Length(0, 100)])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use')
